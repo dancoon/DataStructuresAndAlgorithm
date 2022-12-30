8d8640ae10f1204c7b88954a4874d7d52e68b572
dancoon <dancun.ian@students.jkuat.ac.ke>
2022-12-30 22:07:32 +0300

    Bubble sort in java

---
+++ Sort/Java/BubbleSort.java
@@ -0,0 +1,50 @@
+class BubbleSort{
+	public static class Sort{
+		public void swap(int a, int b){
+			int temp;
+
+			temp = a;
+			a = b;
+			b = temp;
+		}
+
+		public void sortarray(int[] arr, int n){
+			int i, j;
+
+			for(i = 0; i < n; i++){
+				for(j = i + 1; j < n; j++){
+					if (arr[i] > arr[j]){
+						int temp;
+
+						temp = arr[i];
+						arr[i] = arr[j];
+						arr[j] = temp;
+					}
+				}
+			}
+		}
+	
+
+	public void printarray(int[] arr, int n){
+		int i;
+
+		for(i = 0; i < n; i++){
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+}
+	
+	public static void main(String args[]){
+	    int[] arr = {6, 4, 2, 3, 8, 9, 1};
+
+	    Sort obj = new Sort();
+		System.out.println("Array before sorting:");
+		obj.printarray(arr, arr.length);
+
+		obj.sortarray(arr, arr.length);
+		System.out.println("Array before sorting:");
+		obj.printarray(arr, arr.length);
+    }
+}
+
