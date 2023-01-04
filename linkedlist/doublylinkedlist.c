9ebb59ad005ebd577019a02aaf48fe09a39bf519
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-01-04 21:38:54 +0300

    insert_beginning function

--- linkedlist/doublylinkedlist.c
+++ linkedlist/doublylinkedlist.c
@@ -11,12 +11,15 @@ typedef struct node
 Node *head;
 
 void insert_beginning(int item);
+void insert_last(int item);
 
 int main(void)
 {
+    insert_last(4);
     insert_beginning(2);
+    insert_last(3);
     insert_beginning(1);
-    printf("%d %d\n", head->data, head->next->data);
+    printf("%d %d %d %d\n", head->data, head->next->data, head->next->next->data, head->next->next->next->data);
     return (0);
 }
 
@@ -43,4 +46,34 @@ void insert_beginning(int item)
     head = node;
     }
     printf("Node inserted!!!\n");
+}
+
+void insert_last(int item)
+{
+    Node *node = (Node *) malloc(sizeof(Node));
+    Node *current = head;
+
+    if (!node)
+    {
+        printf("Overflow!!!\n");
+        return;
+    }
+    node->data = item;
+    if (head == NULL)
+    {
+        node->next = NULL;
+        node->prev = NULL;
+        head = node;
+    }
+    else
+    {
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+        node->next = current->next;
+        node->prev = current;
+        current->next = node;
+    }
+    printf("Node Inserted!!!\n");    
 }
