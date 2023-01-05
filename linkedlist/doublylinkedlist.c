866fa81b52d3a7684f7f32b022ec6e0f29f5c60a
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-01-05 14:30:10 +0300

    insert_location function

--- linkedlist/doublylinkedlist.c
+++ linkedlist/doublylinkedlist.c
@@ -20,7 +20,8 @@ int main(void)
     insert_beginning(2);
     insert_last(3);
     insert_beginning(1);
-    insert_location(0, 0);    
+    insert_location(0, 7);    
+    // printf("%d", head->next->data);
     printf("%d %d %d %d\n", head->data, head->next->data, head->next->next->data, head->next->next->next->data);
     return (0);
 }
