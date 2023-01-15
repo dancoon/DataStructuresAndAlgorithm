9758e05b82be4292af9f02501e619df6f78c5369
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-01-15 22:17:23 +0300

    Sorting Techniques

---
+++ Sort/C++/Tests/main.h
@@ -0,0 +1,11 @@
+void bubblesort(int arr[], int n);
+void printarray(int arr[], int n);
+void swap(int *a, int *b);
+void insertionsort(int arr[], int n);
+void selectionsort(int arr[], int n);
+void mergesort(int arr[], int beg, int end);
+void quicksort(int arr[], int beg, int end);
+void countingsort(int arr[], int n);
+void radixsort(int arr[], int n);
+void shellsort(int arr[], int n);
+void heapsort(int arr[], int n);
